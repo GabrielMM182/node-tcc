c1405960adfccd2be2a6528a7208fb63ec015ceb
GabrielMM182 <gmm_morais@hotmail.com>
2021-04-15 20:04:05 -0300

    prova

--- app/routes/login.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    
-    const loginController = require('../controllers/login.controller');
-    
-    app.route('/api/logon')
-         .post(loginController.login);
-}
--- app/routes/porcent.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    
-    const porcentController = require('../controllers/porcent.controller');
-    
-    app.route('/api/porcent')
-         .post(porcentController.create)
-         .get(porcentController.getAll);
-
-}
